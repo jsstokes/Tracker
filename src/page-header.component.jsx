d9eda95f2e7eac1235f4a81a5e3fa5e58226b962
Scott Stokes <sstokes99@gmail.com>
2021-05-20 09:59:40 -0500

    added beginnings of a navbar

--- src/page-header.component.jsx
+++ src/page-header.component.jsx
@@ -1,22 +1,37 @@
-import React from 'react';
-import { Pane } from 'evergreen-ui';
+import React from "react";
+import { Button, Pane } from "evergreen-ui";
 
 class PageHeader extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {};
+  }
 
-    constructor( props) {
-        super();
-        this.state = {};
-    }
+  render() {
+    return (
+      <Pane width="100%" align="center">
+        <Pane
+          backgroundColor="lightslategray"
+          width="90%"
+          paddingTop="15px"
+          paddingBottom="15px"
+        >
+          <h1>Tracker</h1>
+        </Pane>
+        <Pane width="90%" 
+          paddingTop="15px"
+          paddingBottom="15px"
+          align="left"
+        >
+            <Button borderRadius="10px" appearance="primary" onClick={this.testClick}>Home</Button>
+            <Button borderRadius="10px">Accounts</Button>
+        </Pane>
+      </Pane>
+    );
+  }
 
-    render() {
-        return(
-            <Pane width="100%" align="center">
-            <Pane backgroundColor="lightslategray" width="90%" paddingTop="15px" paddingBottom="15px">
-                Tracker
-            </Pane>
-          </Pane>
-      
-        );
-    }
+  testClick(event) {
+      console.log("Test Click Fired");
+  }
 }
 export default PageHeader;
